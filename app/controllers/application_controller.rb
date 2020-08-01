ff19998f9fb5377b851e4879405356d9d9c6ee6e
vagrant <vagrant@localhost.localdomain>
2020-08-01 09:30:54 +0000

    [finish]bk2-before_debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,20 @@ class ApplicationController < ActionController::Base
 	before_action :authenticate_user!
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  # アカウント登録後のリダイレクト先
+  def after_sign_up_path_for(resource)
+      user_path(resource)
+  end
+  # ログイン後のリダイレクト先
   def after_sign_in_path_for(resource)
-    root_path
+      user_path(resource)
   end
-
+  # ログアウト後のリダイレクト先
   def after_sign_out_path_for(resource)
-    root_path
+      root_path
   end
 
+  protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
   end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,48 +1,56 @@
 class BooksController < ApplicationController
-
-  def show
-    @book = Book.find(params[:id])
-  end
-
+  
   def index
     @books = Book.all
+    @book_new = Book.new
   end
-
+  
   def create
-    @book = Book.new(book_params)
-    if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+    @book_new = Book.new(book_params)
+    @book_new.user_id = current_user.id
+    if @book_new.save
+      redirect_to book_path(@book_new), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      render :index
     end
   end
 
+  def show
+    @book_new = Book.new
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+  
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
-
-
 
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render "edit"
+      render :edit
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body, :user_id)
   end
+
 
 end
